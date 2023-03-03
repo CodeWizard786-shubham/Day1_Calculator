0e82a3325287810a2a9c3bb139b449701d1f674a
shubham shirke <shirkeshubham84@gmail.com>
2023-03-03 20:42:09 +0530

    Added: Calculator program

---
+++ src/javasamples/HelloWorld.java
@@ -0,0 +1,17 @@
+package javasamples;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        int a=10,b=10;
+        int sum=a+b;
+        int sub=a-b;
+        int mul=a*b;
+        int div=a/b;  //print quotient
+        int mod=a%b;  //print remainder
+        System.out.println("Hello World: "+sum+" times");
+        System.out.println("Hello World: "+sub+" times");
+        System.out.println("Hello World: "+mul+" times");
+        System.out.println("Hello World: "+div+" times");
+        System.out.println("Hello World: "+mod+" times");
+    }
+}
---
+++ src/javasamples/Test.java
@@ -0,0 +1,14 @@
+package javasamples;
+
+public class Test {
+    public static void main(String args[])
+    {
+        int a=30;
+        int b=2;
+        if(a%b==0){
+            System.out.println("2 is the factor of 30");}
+        else {
+            System.out.println("2 is not the factor of 30");
+        }
+    }
+}
