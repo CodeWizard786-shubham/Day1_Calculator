5953738222e91e45b2050fd018561e0d5f106376
shubham shirke <shirkeshubham84@gmail.com>
2023-03-07 19:39:21 +0530

    Added:The switch case statement with the calculator

--- src/javasamples/HelloWorld.java
+++ src/javasamples/HelloWorld.java
@@ -8,30 +8,33 @@ public class HelloWorld {
         double a = sc.nextDouble();
         System.out.println("Enter value for b: ");
         double b = sc.nextDouble();
+        //System.out.println("Enter value for c: ");
+        //char f = sc.next().charAt(0);
         //double a=4.56,b=2.00;
         System.out.println("Enter choice \n 1)Addition \n 2)subtraction \n 3)Multiplication \n 4)Division \n 5)Remainder");
         int choice=sc.nextInt();
-        if(choice==1){
-            double sum=a+b;
-            System.out.println("Addition: "+sum);
-        } else if (choice ==2) {
-            double sub=a-b;
-            System.out.println("Subtraction: "+sub);
-        }
-        else if (choice == 3) {
-            double mul=a*b;
-            System.out.println("Multiplication: "+mul);
-        }
-        else if (choice == 4) {
-            double div=a/b;  //print quotient
-            System.out.println("Division: "+div);
-        }
-        else if (choice == 5) {
-            double mod=a%b;  //print remainder
-            System.out.println("Modulus(Remainder): "+mod);
-        }
-        else{
+        switch(choice){
+            case 1:
+                double sum=a+b;
+                System.out.println("Addition: "+sum);
+                break;
+            case 2:
+                double sub=a-b;
+                System.out.println("Subtraction: "+sub);
+                break;
+            case 3:
+                double mul=a*b;
+                System.out.println("Multiplication: "+mul);
+                break;
+            case 4:
+                double div=a/b;  //print quotient
+                System.out.println("Division: "+div);
+            case 5:
+                double mod=a%b;  //print remainder
+                System.out.println("Modulus(Remainder): "+mod);
+            default:
             System.out.println("Select choice from 1-5");
         }
+        sc.close();
     }
 }
---
+++ src/javasamples/JavaLoops.java
@@ -0,0 +1,7 @@
+package javasamples;
+
+public class JavaLoops {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+    }
+}
